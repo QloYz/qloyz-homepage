074deacea59454fa8ab4c5ba66ae9a16f50ad44f
Henry Grant <henryhpgv5@gmail.com>
2025-09-03 21:16:05 -0500

    fix

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,24 @@
 import Link from 'next/link'
 import { Text, useColorModeValue } from '@chakra-ui/react'
+import styled from '@emotion/styled'
 
+const LogoBox = styled.span`
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
+
+  > svg {
+    transition: 200ms ease;
+  }
+
+  &:hover > svg {
+    transform: rotate(20deg);
+  }
+`
 
 const Logo = () => {
   return (
