0a326480637143f0501f16fa3ef8440063dc63b1
Henry Grant <henryhpgv5@gmail.com>
2025-09-04 23:45:39 -0500

    edit title

--- pages/works/passwordchecker.js
+++ pages/works/passwordchecker.js
@@ -14,7 +14,7 @@ import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 
 const Work = () => (
-  <Layout title="Sorting Visualizer">
+  <Layout title="Password Strength Checker">
     <Container>
       <Title>
         Visualizer for Sorting Algorithms <Badge>2025</Badge>
